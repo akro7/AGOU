d6f6be42ca47a3a4135566c4060e10f84a0cf525
AKRO <habiba31245@gmail.com>
2026-06-16 03:18:14 +0300

    Refactor AkroGramApp to extend ApplicationLoader

--- app/src/main/java/com/akro/agram/AkroGramApp.java
+++ app/src/main/java/com/akro/agram/AkroGramApp.java
@@ -1,26 +1,20 @@
 package com.akro.agram;
 
-import android.app.Application;
 import android.content.Context;
-
 import com.akro.agram.network.TelegramController;
+import org.telegram.messenger.ApplicationLoader;
 
-public class AkroGramApp extends Application {
+public class AkroGramApp extends ApplicationLoader {
 
     private static AkroGramApp instance;
 
-    public static AkroGramApp getInstance() {
-        return instance;
-    }
-
-    public static Context getContext() {
-        return instance.getApplicationContext();
-    }
+    public static AkroGramApp getInstance() { return instance; }
+    public static Context getContext() { return instance.getApplicationContext(); }
 
     @Override
     public void onCreate() {
-        super.onCreate();
         instance = this;
+        super.onCreate(); // الـ ApplicationLoader بيعمل init للـ tgnet هنا
         TelegramController.getInstance().init(this);
     }
 }
